7eee5ea33b8eeb5bff907a6c1ffe9d11c7ab21ee
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-05-12 15:36:56 -0500

    feat: update HomePage layout and title, configure router, and enhance CSS styles

--- src/App/HomePage/index.jsx
+++ src/App/HomePage/index.jsx
@@ -1,5 +1,29 @@
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <article className="p-4 flex flex-col gap-5 justify-center items-center">
+      <header className="">
+        <h2>Bienvenido a tu pagina Educativa</h2>
+      </header>
+      <article className="p-2 grid grid-cols-3 grid-rows-2 gap-5 size-4/5 justify-items-center">
+        {/* 
+          Creo un array de el constructor new Array(5),
+          Este array va a tener un tamaño de 5 (0-5 = 6)
+        */}
+        {Array.from(new Array(5)).map((_,i) => {
+          if (i === 0) return (
+            <section className="bg-amber-800 [grid-area:1/1/1/3] w-full lg:h-[200px] md:h-[150px] sm:h-[100px] rounded-2xl hover:scale-105 hover:bg-amber-800/70">
+            </section>
+          ) 
+          return (
+            <section className="bg-amber-500 rounded-2xl w-full hover:scale-105 hover:bg-amber-500/70">
+            </section>
+          )
+        })}
+        
+      </article>
+
+    </article>
+  );
 };
 
 export default HomePage;
--- src/Router/index.jsx
+++ src/Router/index.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router";
 
 //TODO: Send a Imports to Elements Or Components
+import HomePage from "../App/HomePage";
 
 //? Example how make a Structure on React-Router https://reactrouter.com/start/data/routing#configuring-routes
 const router = createBrowserRouter([
   {
     index: true,
-    // element : HomePage
+    element : <HomePage />,
+    // Component: HomePage
   },
   {
     path: "/log-in",
@@ -16,17 +18,17 @@ const router = createBrowserRouter([
     path: "/sing-up",
     // element : SingUp
   },
-  {
-    path: "/home",
-    // errorElement : Error
-    // element : Layout,
-    children: [
-      {
-        path: "/class/:name",
-        // element : ClassRom
-      },
-    ],
-  },
+  // {
+  //   path: "/home",
+  //   // errorElement : Error
+  //   // element : Layout,
+  //   children: [
+  //     {
+  //       path: "/class/:name",
+  //       // element : ClassRom
+  //     },
+  //   ],
+  // },
 ]);
 
 export default router;
