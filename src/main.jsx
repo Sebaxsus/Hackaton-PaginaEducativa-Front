0ec6313f551e4c8d280e3e0ffe46d7448204ac0a
Sebaxsus <98297670+Sebaxsus@users.noreply.github.com>
2025-05-14 18:20:26 -0500

    Import API KEY FROM .env (Not working hahaha)

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,14 @@ import { RouterProvider } from "react-router";
 import router from "./Router";
 import MainContext from "./Contexts";
 
+// Clerk SDK Import
+import { ClerkProvider } from "@clerk/clerk-react"
+
 import "./base.css";
 
+// TODO: Toca importar el .env, Se me olvido lo de vite.env SJJS 
+const PUBLISHALBE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+console.log("Key?: ", PUBLISHALBE_KEY, import.meta.env.VITE_PRUEBA)
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <MainContext>
