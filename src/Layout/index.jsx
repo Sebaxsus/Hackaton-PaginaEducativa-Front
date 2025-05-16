ce152089ee21371626ad31d5877fbaa4e457f3d8
noneda <nonejda@gmail.com>
2025-05-15 21:53:51 -0500

    Css Typewriter

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -19,16 +19,18 @@ const Layout = () => {
       <PopUp />
       <header className="fixed w-[97.5%] h-auto bg-[var(--Secondary-Color))] m-[1rem] rounded-t-lg flex flex-col gap-[5rem]">
         <section className="w-[100%] h-20 p-10 border-b-1 flex items-center justify-between">
-          <h1 className="font-Limelight text-4xl font-extrabold w-150">
-            <Typewriter text="Classroom" />
-          </h1>
+          <div className="typewriter">
+            <h1 className="font-Limelight text-4xl font-extrabold w-150 justify-self-start self-start">
+              ClassRoom
+            </h1>
+          </div>
           <nav className="flex flex-row items-center gap-[1rem]">
             <ButtonTheme toggle={switchTheme} value={theme} />
             <HeadIcons popUp={showPopUp} />
           </nav>
         </section>
       </header>
-      <main className="h-40"></main>
+      <div className="h-40" />
       <Outlet />
       <footer></footer>
     </>
